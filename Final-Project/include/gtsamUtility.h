a241b227c24962592d9c9032fac80cbe9ed2909e
Chien Erh Lin <chienerh@umich.edu>
2019-04-23 18:43:39 -0400

    added utility

---
+++ Final-Project/include/gtsamUtility.h
@@ -0,0 +1,10 @@
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/Marginals.h>
+#include <gtsam/nonlinear/Values.h>
+
+#include <gtsam/nonlinear/ISAM2.h>
